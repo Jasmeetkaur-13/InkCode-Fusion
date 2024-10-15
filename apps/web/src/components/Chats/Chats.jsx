1e517f9e6bb12a3b2db5abae31f0b7107e7d9d9e
Jasmeet Kaur <143268826+Jasmeetkaur-13@users.noreply.github.com>
2024-10-16 01:54:10 +0530

    Update Chats.jsx

--- apps/web/src/components/Chats/Chats.jsx
+++ apps/web/src/components/Chats/Chats.jsx
@@ -4,22 +4,17 @@ import ACTIONS from "../../Actions";
 import CloseIcon from "@mui/icons-material/Close";
 import SendIcon from "@mui/icons-material/Send";
 import { motion } from "framer-motion";
-import "./Chats.css";
+import "./Chats.css"; 
 import NotificationMessage from "../../pages/Assets/Notification/Notification-Message.mp3";
-
 import preventHorizontalScroll from "../../preventHorizontalScroll";
 
-
-
 const Chat = ({ socket, roomId, userName, setShowChat, isVisible }) => {
-
   useEffect(() => {
     preventHorizontalScroll(); 
   }, []);
-  
+
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
-  console.log(messageList);
   const messagesEndRef = useRef(null);
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -31,10 +26,7 @@ const Chat = ({ socket, roomId, userName, setShowChat, isVisible }) => {
         room: roomId,
         author: userName,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
+        time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
       };
 
       await socket.emit(ACTIONS.SEND_MESSAGE, messageData);
@@ -42,95 +34,66 @@ const Chat = ({ socket, roomId, userName, setShowChat, isVisible }) => {
       setCurrentMessage("");
     }
   };
+
   useEffect(() => {
     if (socket) {
       socket.on(ACTIONS.RECEIVE_MESSAGE, (data) => {
         setMessageList((list) => [...list, data]);
         const audio = new Audio(NotificationMessage);
-        audio.volume = 0.1; // Set the volume to 50%
+        audio.volume = 0.1; // Set the volume to 10%
         audio.play();
       });
     }
   }, [socket]);
+
   useEffect(() => {
     scrollToBottom(); // Scroll to bottom on initial render
-  }, []);
+  }, [messageList]); // Update scroll to bottom when messages change
 
   return (
     <motion.div className={`${isVisible ? "visible" : "hidden"}`}>
-      <div
-        className="flex flex-col absolute z-50 right-[2%] top-[5%] w-auto min-w-[27%] max-w-[27%] h-[84%] flex-shrink-0
-       rounded-2xl bg-White shadow-xl shadow-black "
-      >
-        {" "}
-        <div className="p-6 pr-3 pb-1 pl-5 flex ">
-          <span className="font-Roboto text-base font-medium leading-normal ml-1">
-            In-room messages
-          </span>
-          <CloseIcon
-            className="ml-auto cursor-pointer scale-120 mr-2"
-            onClick={() => setShowChat(false)}
-          />
+      <div className="flex flex-col absolute z-50 right-[2%] top-[5%] w-auto min-w-[27%] max-w-[27%] h-[84%] flex-shrink-0 rounded-2xl bg-White shadow-xl shadow-black">
+        <div className="p-6 pr-3 pb-1 pl-5 flex">
+          <span className="font-Roboto text-base font-medium leading-normal ml-1">In-room messages</span>
+          <CloseIcon className="ml-auto cursor-pointer scale-120 mr-2" onClick={() => setShowChat(false)} />
         </div>
         <div className="flex w-full py-4 px-4 justify-between items-center">
           <div className="flex flex-col w-full h-10 justify-center items-center rounded bg-ChatBg">
-            <span
-              className="flex items-center font-Roboto text-sm xl:text-sm 
-            font-normal"
-            >
-              Main Meet Messages
-            </span>
+            <span className="flex items-center font-Roboto text-sm xl:text-sm font-normal">Main Meet Messages</span>
           </div>
         </div>
-        <div className=" w-full chat-body no-scrollbar flex overflow-hidden relative ">
-          <ScrollToBottom className=" scrollToBottom w-full h-full no-scrollbar custom-scrollbar text-sm font-Roboto mt-6 font-normal ">
-
-            {/* Div especial */}
-            <div className="app-message ">
-            <div
-                className=" flex px-1 py-2 gap-5 rounded-4  
-              xl:w-52 mb-0.5 justify-center w-[full] "
-              >
-                <span
-                  className="text-center text-ChatText font-Roboto text-xs xl:text-xs
-              font-Normal w-[75%]"
-                >
-                  Messages can only be seen by people in the call and are deleted
-                  when the call ends.
+        <div className="w-full chat-body no-scrollbar flex overflow-hidden relative">
+          <ScrollToBottom className="scrollToBottom w-full h-full no-scrollbar custom-scrollbar text-sm font-Roboto mt-6 font-normal">
+            <div className="app-message">
+              <div className="flex px-1 py-2 gap-5 rounded-4 xl:w-52 mb-0.5 justify-center w-full">
+                <span className="text-center text-ChatText font-Roboto text-xs xl:text-xs font-Normal w-[75%]">
+                  Messages can only be seen by people in the call and are deleted when the call ends.
                 </span>
               </div>
             </div>
-              
-              {messageList.map((messageContent) => {
-                return (
-                  <div className="flex justify-end">
-                    <div
-                      className={` message ${
-                        userName === messageContent.author ? "you" : "other"
-                      }`}
-                    >
-                      <div>
-                        <div className="message-content">
-                          <p>{messageContent.message}</p>
-                        </div>
-                      </div>
+
+            {messageList.map((messageContent, index) => {
+              return (
+                <div className="flex justify-end" key={index}>
+                  <div
+                    className={`chat-message ${
+                      userName === messageContent.author ? "you" : "other"
+                    }`}
+                  >
+                    <div className="message-content">
+                      <p>{messageContent.message}</p>
                     </div>
-
-
-                    <div ref={messagesEndRef} />
                   </div>
-                );
-              })}
+                  <div ref={messagesEndRef} />
+                </div>
+              );
+            })}
           </ScrollToBottom>
         </div>
-        <div
-          className="flex items-center relative w-[95%] mb-3 h-14 bg-gray-50 rounded-3xl
-         bg-[#F1F3F4] mx-auto"
-        >
+        <div className="flex items-center relative w-[95%] mb-3 h-14 bg-gray-50 rounded-3xl bg-[#F1F3F4] mx-auto">
           <input
             type="text"
-            className="w-full h-full border-none outline-none px-4 py-3 mt-2 mb-2 text-base 
-            font-roboto font-normal bg-transparent"
+            className="w-full h-full border-none outline-none px-4 py-3 mt-2 mb-2 text-base font-roboto font-normal bg-transparent"
             value={currentMessage}
             placeholder="Send a message to everyone"
             onChange={(event) => {
@@ -146,4 +109,5 @@ const Chat = ({ socket, roomId, userName, setShowChat, isVisible }) => {
     </motion.div>
   );
 };
+
 export default Chat;
